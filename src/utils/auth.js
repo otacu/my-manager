72765bdc81634daf0d5c279849c0a80ab11bec87
JingshengYe <jingsheng-ye@msyc.cc>
2019-09-23 18:06:32 +0800

    :wrench: 给放在localstorage的token设置超时时间

--- src/utils/auth.js
+++ src/utils/auth.js
@@ -1,14 +1,26 @@
-
 const TokenKey = 'my-manager-accessToken'
 
 export function getToken() {
-  return localStorage.getItem(TokenKey)
+    let jsonStr = localStorage.getItem(TokenKey);
+    if (!jsonStr) {
+        return null;
+    }
+    let dataObj = JSON.parse(localStorage.getItem(TokenKey));
+    if (new Date().getTime() > dataObj.time) {
+        localStorage.removeItem(TokenKey);
+        return null;
+    } else {
+        return dataObj.data
+    }
 }
 
 export function setToken(token) {
-  return localStorage.setItem(TokenKey, token)
+    let currentTime = new Date().getTime();
+    // 设置过期时间 (15分钟)
+    let pastTime = 15 * 60 * 1000;
+    return localStorage.setItem(TokenKey, JSON.stringify({data: token, time: currentTime + pastTime}))
 }
 
 export function removeToken() {
-  return localStorage.removeItem(TokenKey)
+    return localStorage.removeItem(TokenKey)
 }
